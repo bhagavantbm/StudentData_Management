97370bbfbc482a685991c550eea6ca4933371891
bhagat <bhagavantmuragundi@gmail.com>
2026-01-27 10:50:30 +0530

    Fixed validation, roll number uniqueness, edit/delete logic

--- script.js
+++ script.js
@@ -6,14 +6,32 @@ const submitBtn = document.querySelector("button");
 
 function saveStudent() {
     let name = document.getElementById("name").value.trim();
-    let roll = document.getElementById("rollnumber").value;
+    let roll = Number(document.getElementById("rollnumber").value);
     let sub1 = Number(document.getElementById("sub1").value);
     let sub2 = Number(document.getElementById("sub2").value);
     let sub3 = Number(document.getElementById("sub3").value);
 
-
  
-    rollNumbers.push(roll);
+ if (!editRowRef) {
+        
+        if (rollNumbers.includes(roll)) {
+            alert("Roll number must be unique!");
+            return;
+        }
+        rollNumbers.push(roll);
+    } else {
+       
+        let oldRoll = Number(editRowRef.children[1].innerText);
+
+        if (oldRoll !== roll && rollNumbers.includes(roll)) {
+            alert("Roll number must be unique!");
+            return;
+        }
+
+       
+        rollNumbers = rollNumbers.filter(r => r !== oldRoll);
+        rollNumbers.push(roll);
+    }
 
 
     let total = sub1 + sub2 + sub3;
@@ -41,7 +59,12 @@ function saveStudent() {
 
     let row = `
         <tr>
-            <td>${name}</td>
+            <td>
+            <button class="btn btn-link p-0"
+                onclick="showStudentCard('${name}','${roll}',${sub1},${sub2},${sub3},${total},${average},'${result}','${Grade}')">
+                ${name}
+            </button>
+            </td>
             <td>${roll}</td>
             <td>${sub1}</td>
             <td>${sub2}</td>
@@ -64,7 +87,6 @@ function saveStudent() {
 
    
     if (editRowRef) {
-        
         editRowRef.children[0].innerText = name;
         editRowRef.children[1].innerText = roll;
         editRowRef.children[2].innerText = sub1;
@@ -80,7 +102,10 @@ function saveStudent() {
          tableBody.innerHTML += row;
     }
 
-        document.querySelector("form").reset();
+        const form = document.querySelector(".needs-validation");
+        form.reset();
+        form.classList.remove("was-validated");
+
 
     
 }
@@ -109,6 +134,30 @@ function editRow(button) {
     document.getElementById("sub3").value =
         editRowRef.children[4].innerText;
 }
+
+function showStudentCard(name, roll, s1, s2, s3, total, avg, result, grade) {
+    document.getElementById("cardName").innerText = name;
+    document.getElementById("cardRoll").innerText = roll;
+    document.getElementById("cardSub1").innerText = s1;
+    document.getElementById("cardSub2").innerText = s2;
+    document.getElementById("cardSub3").innerText = s3;
+    document.getElementById("cardTotal").innerText = total;
+    document.getElementById("cardAverage").innerText = avg.toFixed(2);
+    document.getElementById("cardResult").innerText = result;
+    document.getElementById("cardGrade").innerText = grade;
+
+    document.getElementById("studentCard").classList.remove("d-none");
+}
+
+function closeCard() {
+    document.getElementById("studentCard").classList.add("d-none");
+}
+
+
+
+
+
+
 
 (() => {
     'use strict';
