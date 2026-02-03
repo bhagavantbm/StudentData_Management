986ffe07486fab9bfcd71cfbe16e7b6bbf059c32
bhagat <bhagavantmuragundi@gmail.com>
2026-02-03 18:16:24 +0530

    deleted some files

--- jquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("*").css("background-color", "green");
-    $("#name").css("background-color", "red");
-    $(".dot").bind("click", function () {
-        alert("clicked")
-    })
-});
-
